854a023649167e4a305e130bd13c9270ec44c203
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2021-02-18 10:09:55 +0530

    removed firebase for special purpose

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,15 +3,15 @@ import  {BrowserRouter as Router, Route} from 'react-router-dom';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarMy from "./NavbarMy";
-import Cta from './Cta';
+// import Cta from './Cta';
 import Hero from "./Hero";
 import PreMadeCart from './PreMadeCarts';
 import EachItem from './EachItem';
 import FooterCta from './FooterCta';
 import FooterMy from './FooterMy';
 
-import SignUp from './SignUp'
-import CreateAcc from './CreateAcc';
+// import SignUp from './SignUp'
+// import CreateAcc from './CreateAcc';
 import './App.css';
 
 class App extends React.Component {
